7515ee505d1783ab2f3b4e5d6a327bdbb0c88db7
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-07-23 13:09:29 +0300

    Basic skeleton setup
    created useLLMInference.js for LLM inference logic
    
    Updated AndroidManifest to use GPU access (it's required for Android 12+) - My current emulator is Android 16
    
    Added gamma-2b model to /model directory (I have copied it to /data/local/tmp/llm/model.bin on my emulator device. I will use this path for importing the model)
    
    build.gradle updated to include mediapipe tasks-genai dependency (implementation "com.google.mediapipe:tasks-genai:0.10.11")
    
    Imported expo-dev-client to use development build for expo which will allow use to use native custom module.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import useLLMInference from './lib/useLLMInference';
 
 export default function App() {
+  const test = useLLMInference({modelPath: "/sad/sad"})
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
