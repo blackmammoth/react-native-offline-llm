46afb8656e6dfb4423c41a31c5bb6eb4e53dbd4a
Haileyesus Dessie <118998054+blackmammoth@users.noreply.github.com>
2025-07-28 22:38:52 +0300

    fix: Replaced MainActivity.kt with the previous version to restore functionality

--- android/app/src/main/java/com/blackmammoth/develop/MainActivity.kt
+++ android/app/src/main/java/com/blackmammoth/develop/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ReactActivity() {
     setTheme(R.style.AppTheme);
     super.onCreate(null)
     Log.d("LlmTest", "===JOB BEGAN===")
-    testLlmModel()
+    // testLlmModel()
   }
 
     /**
@@ -57,4 +57,44 @@ class MainActivity : ReactActivity() {
         // Fire off a test request
         llmModel.generateResponseAsync(requestId = 1, prompt = "Who are you?")
     }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "main"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return ReactActivityDelegateWrapper(
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
+  }
+
+  /**
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
+  override fun invokeDefaultOnBackPressed() {
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
+      }
+
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
+  }
 }
